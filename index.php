3d7a08ba5a99c83a3699052422d7baddbeb90cbf
LebonC <lebon.christophe@laposte.net>
2017-05-03 20:02:45 +0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,44 @@
+<?php 	
+
+	require('./controleur/Controleur.php');
+
+  if (isset($_GET['action']))
+  {
+      if ($_GET['action'] == 'Accueil') {
+        Accueil() ; // Acceuil du site
+        exit;
+      }
+       if ($_GET['action'] == 'Rechercher') {
+        Rechercher() ; // Acceuil du site
+        exit;
+      }
+      if ($_GET['action'] == 'Noter') {
+        Noter();
+        exit;
+      }
+       if ($_GET['action'] == 'Admin') {
+        Admin() ; 
+        exit;
+      }
+      if ($_GET['action'] == 'Connexion') {
+        Connexion();
+        exit;
+      }
+      if ($_GET['action'] == 'ValidationConnexion') {
+        ValidationConnexion();
+        exit;
+      }
+      if ($_GET['action'] == 'ValidationInscription') {
+        ValidationInscription();
+        exit;
+      }
+      if ($_GET['action'] == 'Inscription') {
+        Inscription();
+        exit;
+      }
+
+   }
+   else{
+   	 Accueil() ;
+   }
+?>
