27f40d189f3728bf7ea09b60a3d4ad8e39603c48
LebonC <lebon.christophe@laposte.net>
2017-05-16 13:04:41 +0400

    Add files via upload

--- vue/vueNoter.php
+++ vue/vueNoter.php
@@ -1,42 +1,54 @@
 <?php 
-
-$titretab = "Komidi Notes";
-
-ob_start(); 
-foreach($menupage as $page_name => $page_url) 
+session_start();
+if(isset($_SESSION['prenom']))
 {
-	$class = '';
-	if ($current_page == $page_url) {
-		$class = 'active';
+    $titretab = "Komidi Notes";
+
+	ob_start(); 
+	foreach($menupage as $page_name => $page_url) 
+	{
+		$class = '';
+		if ($current_page == $page_url) {
+			$class = 'active';
+		}
+		echo "<li class='$class'>
+			<a href='$page_url'>".$page_name ."</a>".
+		 "</li>";
 	}
-	echo "<li class='$class'>
-		<a href='$page_url'>".$page_name ."</a>".
-	 "</li>";
-}
-$menupage = ob_get_clean();
+		$menupage = ob_get_clean();
 
-ob_start(); 
-echo '
-<!DOCTYPE html>
-<html>
-	<head>
-		<meta charset="utf-8" />
-		<title>Test Notation</title>
-		<link rel="stylesheet" type="text/css" href="Le_CSS_test.css" media="all"/>
-	</head>
+		ob_start(); 
+		echo '
+		<!DOCTYPE html>
+		<html>
+			<head>
+				<meta charset="utf-8" />
+				<title>Test Notation</title>
+				<link rel="stylesheet" type="text/css" href="Le_CSS_test.css" media="all"/>
+			</head>
 
-	<body>
-		<span class="star-rating">  
-			<input type="radio" name="rating" value="1"><i></i>  
-			<input type="radio" name="rating" value="2"><i></i>  
-			<input type="radio" name="rating" value="3"><i></i>  
-			<input type="radio" name="rating" value="4"><i></i>  
-			<input type="radio" name="rating" value="5"><i></i>
-		</span>
-	</body>
-</html>';
-$contenupage = ob_get_clean();
+			<body>
+				<span class="star-rating">  
+					<input type="radio" name="rating" value="1"><i></i>  
+					<input type="radio" name="rating" value="2"><i></i>  
+					<input type="radio" name="rating" value="3"><i></i>  
+					<input type="radio" name="rating" value="4"><i></i>  
+					<input type="radio" name="rating" value="5"><i></i>
+				</span>
+			</body>
+		</html>';
+		$contenupage = ob_get_clean();
 
 require 'vue/vueSidebar.php';
 require 'vue/template.tpl';
+
+
+}
+
+else
+{
+    header("Location:index.php?action=Connexion");
+}
+
+
 ?>
